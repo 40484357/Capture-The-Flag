4c2bb752b9ac7fb783a4af59673f9a1083e83a9e
40484357 <40484357@live.napier.ac.uk>
2023-02-13 09:39:59 +0000

    added comments to describe scene elements in HTML, added script.js and initial code to hide and show scenes with functions that make it easier to script

---
+++ webapp/static/script.js
@@ -0,0 +1,18 @@
+var continueButton = document.getElementById('continue')
+var firstScene = document.getElementById('firstScene')
+var secondScene = document.getElementById('secondScene')
+
+continueButton.addEventListener('click', ()=>{
+    hideScene(firstScene)
+    showScene(secondScene)
+})
+
+function hideScene(Element){
+    Element.classList.add('hidden')
+    Element.classList.remove('gameLauncher')
+} //fast hider of scenes
+
+function showScene(Element){
+    Element.classList.remove('hidden')
+    Element.classList.add('gameLauncher')
+} //fast shower of scenes
