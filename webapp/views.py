61d9a11bd29a7ee3b0f8b0677c83233dc5cf441d
40484357 <40484357@live.napier.ac.uk>
2023-02-13 09:18:26 +0000

    added md5 generator to backend code

--- webapp/views.py
+++ webapp/views.py
@@ -1,9 +1,19 @@
 from flask import Blueprint, render_template, request, redirect, url_for
+import hashlib, random
+
+passwords = ['crypto', 'desktop', 'hash', 'cipher', 'caeser', 'pigpen', 'ethereum']
+
+    
 
 views = Blueprint('views', __name__)
 
 @views.route('/')
 def landing():
+    passLength = len(passwords)
+    selection = random.randint(0, passLength)
+    selected = passwords[selection]
+    password = hashlib.md5(selected.encode())
+    print(password.hexdigest())
     return render_template('cyberescape.html')
 
     
