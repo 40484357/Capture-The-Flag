a03e61b4329b7c854c2a850fde03c54ef6e225cc
40484357 <40484357@live.napier.ac.uk>
2023-02-20 09:19:17 +0000

    change sql key

--- webapp/__init__.py
+++ webapp/__init__.py
@@ -8,7 +8,7 @@ DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'drChickenandtheDuck'
+    app.config['SECRET_KEY'] = 'Amberjack'
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
 
